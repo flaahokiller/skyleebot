cd567fd8650dee65f6ee265905a2401a235e7bb7
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-13 12:43:46 +0530

    Update locks_sql.py

--- skylee/modules/sql/locks_sql.py
+++ skylee/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from skylee.modules.sql import SESSION, BASE
+from saber.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
