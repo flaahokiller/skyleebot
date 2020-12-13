d9315a034075d848736c9e88701b0c36b2099636
Hyper <70685160+hyper-ub@users.noreply.github.com>
2020-12-13 12:45:38 +0530

    Update userinfo_sql.py

--- skylee/modules/sql/userinfo_sql.py
+++ skylee/modules/sql/userinfo_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, UnicodeText
 
-from skylee.modules.sql import SESSION, BASE
+from saber.modules.sql import SESSION, BASE
 
 
 class UserInfo(BASE):
